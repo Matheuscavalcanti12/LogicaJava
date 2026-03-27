2743aa7f76079f37f0fb22d3e8e168510c8f3903
Matheuscavalcanti12 <matheuscavalcanti838@gmail.com>
2026-03-27 16:59:02 -0300

    Exercicio1, alterando nomenclatura

--- Exercicios/Exercicio1.java
+++ Exercicios/Exercicio1.java
@@ -1,8 +1,8 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 import java.util.Scanner;
-
-public class Main {
+//exercicio 1.
+public class Exercicio1 {
 
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
